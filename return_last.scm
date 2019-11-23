da7e08f62493e3f992a1c78d85fb72259b25f732
A.M.Nolan <username@users.noreply.github.com>
2019-11-23 06:10:06 -0700

    Removed length function call

--- return_last.scm
+++ return_last.scm
@@ -2,7 +2,6 @@
 ((= 1 (length lst)) lst)
 (else (retLast (cdr lst)))))
 
-(print (length '(1 2 3 0 4 5 6 0 7)))
 (print (retLast '(1 2 3 0 4 5 6 0 7)))
 (print (retLast '()))
 (print (retLast '(8 9)))
