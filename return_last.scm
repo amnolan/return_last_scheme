e629e762740f1e653f6182171240b7aa98910a21
A.M.Nolan <username@users.noreply.github.com>
2019-11-23 06:06:41 -0700

    First commit

---
+++ return_last.scm
@@ -0,0 +1,8 @@
+(define(retLast lst)(cond((null? lst) '())
+((= 1 (length lst)) lst)
+(else (retLast (cdr lst)))))
+
+(print (length '(1 2 3 0 4 5 6 0 7)))
+(print (retLast '(1 2 3 0 4 5 6 0 7)))
+(print (retLast '()))
+(print (retLast '(8 9)))
